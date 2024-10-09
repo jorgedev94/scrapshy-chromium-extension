a4a079f90e0986fc8bff11a5093ede130f60803f
smoncada19 <santiago.mp08@hotmail.com>
2024-10-09 12:34:04 -0500

    ok - firstname, middlename, lastname of owner

--- angular/src/app/services/scrap.service.ts
+++ angular/src/app/services/scrap.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { last } from 'rxjs';
 
 interface ScrapData {
   efectividad: string[];
@@ -10,6 +11,9 @@ interface ScrapData {
   owner: string;
   email?: string; // Opcional
   phone?: string; // Opcional
+  firstname: string;
+  lastname: string;
+  middlename: string;
 }
 
 @Injectable({
@@ -62,22 +66,48 @@ export class Scrapshy {
                                 const elementValues4 = getSpanTexts(texts4);
                                 const elementValues5 = getSpanTexts(texts5);
                                 const elementValues6 = getSpanTexts(texts6);
-                                const elementValues7 = document.querySelector('.css-1ahwws6').textContent.trim()
+                                const elementValues7 = document.querySelector('.css-1ahwws6').textContent.trim()                            
 
-                                let data = {
+                                let data = {                                    
                                     miembros: elementValues,
                                     efectividad: elementValues2,
                                     terminacion: elementValues3,
                                     subscriber_id: elementValues4,
                                     policy_id: elementValues5,
                                     ffm_id: elementValues6,
-                                    owner: elementValues7,                                    
+                                    owner: elementValues7,         
                                     email: "example@example.com", // Añadir si es necesario
-                                    phone: "123-456-7890"
+                                    phone: "123-456-7890",
+                                    firstname: '',
+                                    lastname: '',
+                                    middlename: ''
                                 };
 
                                 if (data) {
                                     const namesArray = data.miembros[0].split(',').map((name) => name.trim());
+                                    const nombreCompleto = data.owner.split(' ');
+                                    const cantidadnombres = nombreCompleto.length;
+                                    data.firstname = nombreCompleto[0]
+                                    
+                                    if(cantidadnombres>0 && cantidadnombres<2 && nombreCompleto[1].length<2 || cantidadnombres>3){
+                                        const secondname = nombreCompleto[1]
+                                        data.middlename = secondname
+                                        if(cantidadnombres>2){
+                                            const apellido = (nombreCompleto[2]+' '+nombreCompleto[3])
+                                            data.lastname = apellido
+                                        }else{
+                                            const apellido = nombreCompleto[2]
+                                            data.lastname = apellido
+                                        }
+                                    }else if(cantidadnombres>0 && cantidadnombres<2){
+                                        const apellido = nombreCompleto[1]
+                                        data.lastname = apellido
+                                    }else{
+                                        const apellido = (nombreCompleto[1]+' '+nombreCompleto[2])
+                                        data.lastname = apellido
+                                    }
+                                    
+                                    data.owner = nombreCompleto[0]
                                     data.miembros = namesArray;
                                 }
                                 return data;
